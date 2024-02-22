e45e7918708a0b8b7e23137e837cca3c8fe15a5b
Grace Muthui <muthuigracia27@gmail.com>
2024-02-23 02:22:52 +0300

    Experience section and json files

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from './Experience.module.css'
+import skills from '../../Data/skills.json'
+
+const Experience = () => {
+  return (
+    <section id='experience'>
+        <h2>Experience</h2>
+        <div>
+            <div></div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Experience
