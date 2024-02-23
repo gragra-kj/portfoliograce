b6916710f089120a7a8b3634eff2d55d4a77af05
Grace Muthui <muthuigracia27@gmail.com>
2024-02-23 23:06:32 +0300

    Exoerience section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./App.module.css"
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import Experience from './components/Experience/Experience'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Hero/>
       <About/>
+      <Experience/>
     </div>
   )
 }
